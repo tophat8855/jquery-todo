6c9c962ca8df3c167b14b06d2b4ab0ed063bc98a
tophat8855 <hrfarley@gmail.com>
2015-01-22 13:47:05 -0800

    Looks good, but not functional

--- todo.js
+++ todo.js
@@ -1,5 +1,12 @@
 $( document ).ready(function() {
   console.log("Working");
   $('body').append($('<h1>App of Todo-ness</h1>'));
-  $('h1').append($('<input placeholder="Whatcha wanna do?"></input>'))
+  $('h1').append($('<input placeholder="Whatcha wanna do?"></input>'));
+
+  $('body').append($('<ul>'));
+  $('ul').append($('<li>Thing?<span class="delete">Delete</span></li>'));
+  $('ul').append($('<li>Another thing?<span class="delete">Delete</span></li>'));
+
+  $('body').append($('<p id="itemsLeft">number left </p>'));
+  $('body').append($('<button id="deleteAll">Exterminate Completed</button>'));
 });
