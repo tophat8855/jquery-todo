6c8fba20710d3457f0116dc372eb215f65d21ced
tophat8855 <hrfarley@gmail.com>
2015-01-22 13:21:27 -0800

    header, input box

---
+++ todo.js
@@ -0,0 +1,5 @@
+$( document ).ready(function() {
+  console.log("Working");
+  $('body').append($('<h1>App of Todo-ness</h1>'));
+  $('h1').append($('<input placeholder="Whatcha wanna do?"></input>'))
+});
