0573e716a22eede57f1d0f72ac73f198fb8a20ff
tophat8855 <hrfarley@gmail.com>
2015-01-22 14:54:28 -0800

    adds new tasks, removes tasks, mostly functional

--- todo.js
+++ todo.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
   console.log("Working");
   $('body').append($('<h1>App of Todo-ness</h1>'));
-  $('h1').append($('<input placeholder="Whatcha wanna do?"></input>'));
+  $('body').append($('<input placeholder="Whatcha wanna do?"></input>'));
 
   $('body').append($('<ul>'));
   $('ul').append($('<li>Thing?<span class="delete">Delete</span></li>'));
@@ -9,4 +9,31 @@ $( document ).ready(function() {
 
   $('body').append($('<p id="itemsLeft">number left </p>'));
   $('body').append($('<button id="deleteAll">Exterminate Completed</button>'));
+
+  var addAThing = function(thingToDo) {
+    var newThingTodo = '<li>' + thingToDo + '<span class="delete">Delete</span></li>';
+    $('ul').append($(newThingTodo));
+  };
+
+  $('input').keypress(function(e) {
+    if (e.which == 13) {
+      var newTodo = $('input').val();
+      addAThing(newTodo);
+      $('input').val("");
+    }
+  });
+
+  $('ul').on('click', 'li', function() {
+    $(this).toggleClass('completed');
+  });
+
+  $('ul').on('click', 'span', function(){
+    $(this).parent().remove();
+  });
+
+  $('#deleteAll').on('click', function() {
+    $('li.completed').fadeOut(300, function() {
+      $(this).remove();
+    });
+  });
 });
